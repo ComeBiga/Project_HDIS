07201ab36190d3a9fe6c0ff893387b28dfab0281
이재환 <36800639+ComeBiga@users.noreply.github.com>
2025-08-12 13:51:10 +0900

    PlayerMoveState 주석 정리

--- Project_HDIS/Assets/1.Scripts/PlayerMoveState.cs
+++ Project_HDIS/Assets/1.Scripts/PlayerMoveState.cs
@@ -6,10 +6,9 @@ using static PlayerMovement;
 
 public class PlayerMoveState : PlayerStateBase
 {
-    private Vector3 mPreviousForward;
-    private float mRotationDirection;
-    private bool mbChangeDirection = false;
-    private bool mbRotating = false;
+    private Vector3 mPreviousForward;       // 회전을 시작하면 어느 방향으로 도는지 체크해야되기 때문에 이전 방향을 저장
+    private bool mbDirectionChanged = false;
+    private bool mbRotating = false;        // 현재 사용되는 곳은 없지만 회전을 체크하는 변수이기 때문에 유지
 
     public override void EnterState()
     {
@@ -29,55 +28,62 @@ public class PlayerMoveState : PlayerStateBase
         mController.Animator.SetInputXMagnitude(Mathf.Abs(mController.InputHandler.MoveInput.x));
 
         // Set Direction
+        // 키입력이 들어오고 방향이 바뀌는 찰나 시점에 대한 코드
         if (mController.InputHandler.MoveInput.x > .001f || mController.InputHandler.MoveInput.x < -.001f)
         {
             EDirection targetDirection = mController.InputHandler.MoveInput.x > 0f ? EDirection.Right : EDirection.Left;
 
             if (targetDirection != mController.Movement.Direction)
             {
-                mbChangeDirection = true;
+                mbDirectionChanged = true;
+                // AnimatorController를 새로 구성하고 사용되지 않는 Parameter
                 mController.Animator.Turning(true);
             }
             else
             {
+                // 방향이 바뀌기 시작하는 프레임에 transition condition 구분을 주려고
+                // if-else로 나눈 거 같은데 지금은 영향이 없는 듯하다
+                // 그리고 지금 애초에 사용되지 않는 Parameter
                 mController.Animator.SetHorizontal(mController.InputHandler.MoveInput.x);
             }
 
+            // 방향이 바뀔 때 한 번 설정해주면 되기 때문에 이 라인도 if문 안에 넣어주면 될 듯 하다
             mController.Movement.SetDirection(targetDirection);
         }
 
-        // Turn Animation
+        // Turn CW/CCW
         Vector3 currentForward = transform.forward;
-
-        mRotationDirection = Vector3.SignedAngle(mPreviousForward, currentForward, Vector3.up);
-
-        if (mbChangeDirection)
+        float deltaRotatedAngle = Vector3.SignedAngle(mPreviousForward, currentForward, Vector3.up);
+        
+        // 회전 시작하면 어느 방향 회전인지 체크 후 Turn 애니메이션 전환
+        if (mbDirectionChanged)
         {
-            if (mRotationDirection < -5f)
+            // 반시계방향 회전 트리거
+            if (deltaRotatedAngle < -5f)
             {
                 mController.Animator.TurnL(true);
                 mController.Animator.TurnR(false);
-                mController.Animator.Turning(false);
-                mbChangeDirection = false;
+                // mController.Animator.Turning(false);
+                mbDirectionChanged = false;
+
+                // 회전 각도가 있으면 회전이라고 의도를 갖게 되는데
+                // 지금은 사용되는 곳이 없지만 혹시 사용되면 신경을 써야할 것 같다
                 mbRotating = true;
             }
-            else if (mRotationDirection > 5f)
+            // 시계방향 회전 트리거
+            else if (deltaRotatedAngle > 5f)
             {
                 mController.Animator.TurnL(false);
                 mController.Animator.TurnR(true);
-                mController.Animator.Turning(false);
-                mbChangeDirection = false;
+                // mController.Animator.Turning(false);
+                mbDirectionChanged = false;
                 mbRotating = true;
             }
-            //else
-            //{
-            //    mController.Animator.TurnL(false);
-            //    mController.Animator.TurnR(false);
-            //}
         }
+        // 회전 방향 체크 중이 아니면 
         else
         {
-            if (mRotationDirection > -1f && mRotationDirection < 1f)
+            if (deltaRotatedAngle > -1f && deltaRotatedAngle < 1f)
             {
                 mController.Animator.TurnL(false);
                 mController.Animator.TurnR(false);
@@ -93,31 +99,17 @@ public class PlayerMoveState : PlayerStateBase
         // Jump
         if (mController.InputHandler.JumpPressed)
         {
-            //if(mController.InputHandler.MoveInput.x > .3f || mController.InputHandler.MoveInput.x < -.3f)
-            //{
-            //    mController.StateMachine.JumpState.type = PlayerJumpState.EType.Run;
-            //}
-            //else
-            //{
-            //    mController.StateMachine.JumpState.type = PlayerJumpState.EType.Idle;
-
-            // if(mController.Animator.IsCurrentState(mController.Animator.RunStateHash))
+            // 점프 입력이 됐을 때 이동 입력이 있으면 무조건 RunJump
             if (mController.InputHandler.MoveInput.x > .01f || mController.InputHandler.MoveInput.x < -.01f)
             {
-                // mController.StateMachine.JumpState.type = PlayerJumpState.EType.Run;
-
                 mController.StateMachine.SwitchState(PlayerStateMachine.EState.RunJump);
                 mController.InputHandler.ResetJump();
             }
-            //else if (mController.InputHandler.MoveInput.x < .3f && mController.InputHandler.MoveInput.x > -.3f)
             else
             {
-                // mController.StateMachine.JumpState.type = PlayerJumpState.EType.Idle;
-
                 mController.StateMachine.SwitchState(PlayerStateMachine.EState.IdleJump);
                 mController.InputHandler.ResetJump();
             }
-
         }
 
         // Ladder
@@ -134,6 +126,7 @@ public class PlayerMoveState : PlayerStateBase
                     PlayerLadderState ladderStateBase = mController.StateMachine.GetStateBase(PlayerStateMachine.EState.Ladder) as PlayerLadderState;
                     LadderHandler ladderHandler = ladderCollider.GetComponent<LadderHandler>();
 
+                    // Top에서 위 키 입력했을 때 사다리 타는 걸 방지하기 위함
                     if (ladderStateBase.IsOverRange(ladderHandler))
                         continue;
 
