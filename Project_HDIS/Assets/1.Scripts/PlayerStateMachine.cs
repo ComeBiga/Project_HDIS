a9109010786faf2841a976d3ba23d310510c14f4
이재환 <36800639+ComeBiga@users.noreply.github.com>
2025-08-13 22:36:58 +0900

    PlayerStateMachine 및 PlayerChangeDirectionState 코드 정리

--- Project_HDIS/Assets/1.Scripts/PlayerStateMachine.cs
+++ Project_HDIS/Assets/1.Scripts/PlayerStateMachine.cs
@@ -10,21 +10,12 @@ public class PlayerStateMachine : MonoBehaviour
 
     public EState CurrentState => mCurrentState;
     public PlayerStateBase CurrentStateBase => mCurrentStateBase;
-    [Obsolete] public PlayerMoveState MoveState => _moveState;
-    [Obsolete] public PlayerJumpState JumpState => _jumpState;
-    [Obsolete] public PlayerRunJumpState RunJumpState => _runJumpState;
-    [Obsolete] public PlayerChangeDirectionState ChangeDirectionState => _changeDirectionState;
 
     [SerializeField] private List<PlayerStateBase> _states = new List<PlayerStateBase>();
-    [SerializeField] private PlayerMoveState _moveState;
-    [SerializeField] private PlayerJumpState _jumpState;
-    [SerializeField] private PlayerRunJumpState _runJumpState;
-    [SerializeField] private PlayerChangeDirectionState _changeDirectionState;
 
     private PlayerController mController;
     private EState mCurrentState;
     private PlayerStateBase mCurrentStateBase;
-    private List<PlayerStateBase> mStates = new List<PlayerStateBase>(10);
     private Dictionary<EState, PlayerStateBase> mStateDic = new Dictionary<EState, PlayerStateBase>(10);
 
     public PlayerStateBase GetStateBase(EState state)
@@ -37,18 +28,9 @@ public class PlayerStateMachine : MonoBehaviour
         state.Initialize(mController);
         mStateDic.Add(state.key, state);
     }
-
-    [Obsolete]
-    public void SwitchState(PlayerStateBase state)
-    {
-        mCurrentStateBase?.ExitState();
-        mCurrentStateBase = state;
-        mCurrentStateBase.EnterState();
-    }
     
     public PlayerStateBase SwitchState(EState state)
     {
-
         mCurrentStateBase?.ExitState();
         mCurrentStateBase = mStateDic[state];
         mCurrentState = state;
@@ -63,21 +45,9 @@ public class PlayerStateMachine : MonoBehaviour
     {
         mController = GetComponent<PlayerController>();
 
-        mStates.Add(_moveState);
-        mStates.Add(_jumpState);
-        mStates.Add(_runJumpState);
-        mStates.Add(_changeDirectionState);
-
-        foreach(var state in mStates)
-        {
-            state.Initialize(mController);
-        }
-
         foreach(var state in _states)
         {
             ResisterState(state);
-            //mStateDic.Add(state.key, state);
-            //state.Initialize(mController);
         }
     }
 }
